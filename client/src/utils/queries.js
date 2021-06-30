959c11992c41da9fd667e1212ea4d1d659fb3078
MeganClose <megs.close@gmail.com>
2021-06-30 14:53:48 -0700

    had to change query to get login to work

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,23 @@
 import gql from 'graphql-tag';
 
+// export const LOGIN_USER = gql`
+//   query login($email: String!, $password: String!) {
+//     login(email: $email, password: $password) {
+//       token
+//       user {
+//         _id
+//         username
+//       }
+//     }
+//   }
+// `;
+
+
 export const LOGIN_USER = gql`
   query login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
-        _id
-        username
-      }
+      username
     }
   }
 `;
