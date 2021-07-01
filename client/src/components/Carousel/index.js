173648fd5a0f694cd539a7fc9ca9cf255ecb2079
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-30 22:03:08 -0700

    added components for tooltip commented out for now, and tip values for both business/fun logos

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -7,6 +7,33 @@ import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import funLogos from './funlogos';
 
+
+//https://material-ui.com/components/tooltips/
+// import Fab from '@material-ui/core/Fab';
+// import Tooltip from '@material-ui/core/Tooltip';
+
+// const useStyles = makeStyles((theme) => ({
+//   fab: {
+//     margin: theme.spacing(2),
+//   },
+//   absolute: {
+//     position: 'absolute',
+//     bottom: theme.spacing(2),
+//     right: theme.spacing(3),
+//   },
+// }));
+
+// export default function SimpleTooltips() {
+//   const classes = useStyles();
+
+
+/* <Tooltip title="Add" aria-label="add">
+<Fab color="primary" className={classes.fab}>
+  <AddIcon />
+</Fab>
+</Tooltip> */
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
